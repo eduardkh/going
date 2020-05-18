dfb8a774008b20c8440d7c3480715dea0514f3ab
Eduard Khiaev <eddietheone1984@gmail.com>
2020-05-18 14:52:38 +0300

    channel-synchronization

---
+++ Go by Example/20 Channel Synchronization/channel-synchronization.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// https://gobyexample.com/channel-synchronization
+
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second * 1)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func main() {
+
+	done := make(chan bool, 1)
+	go worker(done)
+
+	<-done
+}
