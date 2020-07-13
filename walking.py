83901f71a74a64c747e474bdf2cc89762c10b943
Eduard Khiaev <eddietheone1984@gmail.com>
2020-07-13 12:00:01 +0300

    python - _ffmpeg

--- walking.py
+++ walking.py
@@ -1,19 +1,16 @@
 import os
-from os.path import isfile
+import subprocess
 
 path = 'c:\\suka\\'
-
+_ffmpeg = "C:\\ffmpeg-20200628-4cfcfb3-win64-static\\bin\\ffmpeg.exe"
 files = []
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
     for _file in f:
-        if '.txt' in _file:
+        if '.mp4' in _file:
             files.append(os.path.join(r, _file))
 
-counter = 1
 for f in files:
-    base = '\\'.join(f.split('\\')[0:-1])
-    new_file = f.split('\\')[-1]
-    print(f'{base}\\new_name_{counter}.txt')
-    os.rename(f, f'{base}\\new_name_{counter}.txt')
-    counter += 1
+    # print(f)
+    # print('{} -i "{}" -c:v libx264 -c:a libmp3lame -b:a 384K "{}.avi"'.format(_ffmpeg, f, f))
+    subprocess.Popen('{} -i "{}" -c:v libx264 -c:a libmp3lame -b:a 384K "{}.avi"'.format(_ffmpeg, f, f))
