b6fce3a705b92b986f9964e23cdb3cab629ad640
Eduard Khiaev <eddietheone1984@gmail.com>
2020-07-13 09:01:58 +0300

    walking - fixed

--- walking.py
+++ walking.py
@@ -10,11 +10,10 @@ for r, d, f in os.walk(path):
         if '.txt' in _file:
             files.append(os.path.join(r, _file))
 
+counter = 1
 for f in files:
-    counter = 12
     base = '\\'.join(f.split('\\')[0:-1])
     new_file = f.split('\\')[-1]
-    # print(f'{base}\\{new_file}')
-    # print(base + '\\new_name_' + str(counter))
-    os.rename(f, base + '\\new_name_' + str(counter))
-    counter = counter + 1
+    print(f'{base}\\new_name_{counter}.txt')
+    os.rename(f, f'{base}\\new_name_{counter}.txt')
+    counter += 1
