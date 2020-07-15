cea8cb2922e77be832de21fff2dbcd2b892d751e
Eduard Khiaev <eddietheone1984@gmail.com>
2020-07-15 14:20:07 +0300

    , cookies=cookies --

--- DVWA/command_execution.py
+++ DVWA/command_execution.py
@@ -16,11 +16,11 @@ DATA2 = {
     'password': 'password',
     'Login': 'Login',
 }
-res2 = _session.post(url=URL2, data=DATA2, cookies=cookies)
+res2 = _session.post(url=URL2, data=DATA2)
 
 """get the correct URL page"""
 URL3 = 'http://192.168.56.132/dvwa/vulnerabilities/exec/'
-res3 = _session.get(url=URL3, cookies=cookies)
+res3 = _session.get(url=URL3)
 if 'Ping for FREE' in res3.text:
     print('ok')
 
@@ -30,6 +30,6 @@ DATA4 = {
     'ip': '127.0.0.1',
     'submit': 'submit',
 }
-res4 = _session.post(url=URL4, data=DATA4, cookies=cookies)
+res4 = _session.post(url=URL4, data=DATA4)
 if '64 bytes from 127.0.0.1' in res4.text:
     print('ok')
