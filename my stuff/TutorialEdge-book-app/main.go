e386f1637c65f7bcf1739def400e9df2d0ecd2c1
Eduard Khiaev <eddietheone1984@gmail.com>
2021-06-13 11:19:10 +0300

    TutorialEdge-book-app book.go + postman collection

--- my stuff/TutorialEdge-book-app/main.go
+++ my stuff/TutorialEdge-book-app/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-    "log"
+	"TutorialEdge-book-app/book"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
 func main() {
-    app := fiber.New()
-
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
-
-    log.Fatal(app.Listen(":3000"))
+	app := fiber.New()
+	setupRoutes(app)
+	log.Fatal(app.Listen(":3000"))
 }
