765ac647cabd809cb008c388964a0f99f1d12023
Eduard Khiaev <eddietheone1984@gmail.com>
2021-06-14 14:59:17 +0300

    cobracli - hostname

--- my stuff/cobracli/cmd/hostname.go
+++ my stuff/cobracli/cmd/hostname.go
@@ -17,6 +17,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -24,15 +25,17 @@ import (
 // hostnameCmd represents the hostname command
 var hostnameCmd = &cobra.Command{
 	Use:   "hostname",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "get OS hostname\n",
+	Long: `📘 get the current OS hostname
+📝 example usage: cobracli hostname
+`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hostname called")
+		name, err := os.Hostname()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("🤖 hostname:", name)
 	},
 }
 
