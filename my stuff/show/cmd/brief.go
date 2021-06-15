aa6e782aefb9af107ff94bcfb1d578c5030a4b50
Eduard Khiaev <eddietheone1984@gmail.com>
2021-06-15 08:24:46 +0300

    show - dummy data

--- my stuff/show/cmd/brief.go
+++ my stuff/show/cmd/brief.go
@@ -17,9 +17,19 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("brief called")
+		fmt.Println(briefDummy)
 	},
 }
+
+var briefDummy string = `
+Interface                  IP-Address      OK? Method Status                Protocol
+GigabitEthernet0/0         unassigned      YES NVRAM  administratively down down
+GigabitEthernet0/1         13.0.0.3        YES NVRAM  up                    up
+GigabitEthernet0/2         unassigned      YES NVRAM  administratively down down
+GigabitEthernet0/3         unassigned      YES NVRAM  administratively down down
+Loopback0                  3.3.3.3         YES NVRAM  up                    up
+
+`
 
 func init() {
 	interfaceCmd.AddCommand(briefCmd)
