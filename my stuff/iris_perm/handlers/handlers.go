bdbe342152997bafc2f6f4bb52e62a746a78d751
Eduard Khiaev <eddietheone1984@gmail.com>
2021-10-25 16:53:20 +0000

    add footer

--- my stuff/iris_perm/handlers/handlers.go
+++ my stuff/iris_perm/handlers/handlers.go
@@ -7,30 +7,33 @@ import (
 )
 
 var startTime = time.Now()
+var year = startTime.Year()
 
 func Index(ctx iris.Context) {
 
 	ctx.View("index.html", iris.Map{
 		"Title":           "Page Title",
-		"FooterText":      "Footer contents",
 		"Message":         "Main contents",
 		"Name":            "iris",
 		"serverStartTime": startTime,
+		"FooterYear":      year,
 	})
 }
 
 func GetBooks(ctx iris.Context) {
 
 	ctx.View("books.html", iris.Map{
-		"Title":   "Page Title",
-		"Message": "books name",
+		"Title":      "Page Title",
+		"Message":    "books name",
+		"FooterYear": year,
 	})
 }
 
 func GetBook(ctx iris.Context) {
 
 	ctx.View("book.html", iris.Map{
-		"Title":   "Page Title",
-		"Message": "ספר",
+		"Title":      "Page Title",
+		"Message":    "ספר",
+		"FooterYear": year,
 	})
 }
