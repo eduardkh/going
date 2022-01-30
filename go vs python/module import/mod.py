ebda17835f64e73776e8dd3618d949404179501d
Eduard Khiaev <eddietheone1984@gmail.com>
2022-01-30 09:02:00 +0200

    first_function as a parameter

--- go vs python/module import/mod.py
+++ go vs python/module import/mod.py
@@ -1,5 +1,18 @@
+
+"""
+second_function doesn't use anything from the main module
+it is called with no parameters
+"""
+
+
 def second_function():
     print("This is the second function")
+
+
+"""
+third_function uses first_function from the main module
+it is called with first_function as a parameter
+"""
 
 
 def third_function(first_function):
