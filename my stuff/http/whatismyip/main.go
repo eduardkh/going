9e8b552590f819109de43b6b4b697ad7c86f1ad1
Eduard Khiaev <eddietheone1984@gmail.com>
2022-04-11 17:01:42 +0000

    log

--- my stuff/http/whatismyip/main.go
+++ my stuff/http/whatismyip/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", strings.Split(r.RemoteAddr, ":")[0])
 }
 
 func main() {
-	http.HandleFunc("/", greet)
+	http.HandleFunc("/", index)
+	log.Println("whatismyip Listening on 8080...")
 	http.ListenAndServe(":8080", nil)
 }
