2665490233fb20b8aef74cdbb301b5109d1491cd
Eduard Khiaev <eddietheone1984@gmail.com>
2022-06-02 13:30:55 +0000

    add jet template

--- my stuff/wschat/handlers/handlers.go
+++ my stuff/wschat/handlers/handlers.go
@@ -1,9 +1,35 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/CloudyKit/jet/v6"
 )
 
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode(),
+)
+
+func renderPage(w http.ResponseWriter, template string, data jet.VarMap) error {
+	view, err := views.GetTemplate(template)
+	if err != nil {
+		log.Println(err)
+		// http.Error(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to WSChat!"))
+	err := renderPage(w, "home.gohtml", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
