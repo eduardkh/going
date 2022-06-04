957d51b721d594d77f66bbb70767f1274b591eb6
Eduard Khiaev <eddietheone1984@gmail.com>
2022-06-04 14:18:52 +0000

    add static file server with pat

--- my stuff/wschat/cmd/web/routes.go
+++ my stuff/wschat/cmd/web/routes.go
@@ -7,9 +7,13 @@ import (
 	"github.com/bmizerany/pat"
 )
 
+// https://mpolinowski.github.io/devnotes/2021-09-10--websocket-recconects
+var fs = http.FileServer(http.Dir("./static/"))
+
 func routes() http.Handler {
 	mux := pat.New()
 	mux.Get("/", http.HandlerFunc(handlers.Home))
 	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+	mux.Get("/static/", http.StripPrefix("/static", fs))
 	return mux
 }
