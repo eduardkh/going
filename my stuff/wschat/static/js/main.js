bb588909649ed52df2c017a4ca00b9b934958312
Eduard Khiaev <eddietheone1984@gmail.com>
2022-06-04 14:59:05 +0000

    connect to sw endpoint from js

--- my stuff/wschat/static/js/main.js
+++ my stuff/wschat/static/js/main.js
@@ -1,3 +1,8 @@
-window.onload = function() {
-    alert("Hello World");
-};
+let socket = null;
+
+document.addEventListener('DOMContentLoaded', () => {
+    socket = new WebSocket("ws://192.168.1.155:8080/ws");
+    socket.onopen = () => {
+        console.log("Successfully connected");
+    };
+});
