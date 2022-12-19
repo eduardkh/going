9edbd8aaaf83f3c7ce2016a5da070eebc6e06720
Eduard Khiaev <eddietheone1984@gmail.com>
2022-12-19 15:10:46 +0200

    show ip gateway

---
+++ my stuff/show/cmd/gateway.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/bi-zone/wmi"
+	"github.com/spf13/cobra"
+)
+
+type Win32_NetworkAdapterConfiguration struct {
+	Description      string
+	DefaultIPGateway []string
+	IPAddress        []string
+	IPSubnet         []string
+}
+
+// gatewayCmd represents the gateway command
+var gatewayCmd = &cobra.Command{
+	Use:   "gateway",
+	Short: "get default gateway address",
+	Long:  `show ip gateway gives you back the the default gateway address`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var dst []Win32_NetworkAdapterConfiguration
+
+		q := wmi.CreateQueryFrom(&dst, "Win32_NetworkAdapterConfiguration", "")
+		if err := wmi.Query(q, &dst); err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range dst {
+			if len(v.DefaultIPGateway) != 0 {
+				fmt.Println("IP Address\tSubnet Mask\tDefault Gateway\tInterface Description")
+				fmt.Printf("%v\t%v\t%v\t%v\n", v.IPAddress[0], v.IPSubnet[0], v.DefaultIPGateway[0], v.Description)
+			}
+		}
+	},
+}
+
+func init() {
+	ipCmd.AddCommand(gatewayCmd)
+}
--- my stuff/show/go.mod
+++ my stuff/show/go.mod
@@ -3,6 +3,7 @@ module show
 go 1.16
 
 require (
+	github.com/bi-zone/wmi v1.1.4 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
