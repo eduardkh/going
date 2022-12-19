d1de2a4d64e8f4a0a1227f0c142ec07b29c909bd
Eduard Khiaev <eddietheone1984@gmail.com>
2022-12-19 22:09:00 +0200

    ip interface

--- my stuff/show/cmd/interface.go
+++ my stuff/show/cmd/interface.go
@@ -2,35 +2,46 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/bi-zone/wmi"
 	"github.com/spf13/cobra"
 )
+
+type interface_win32_NetworkAdapterConfiguration struct {
+	Description      string
+	DefaultIPGateway []string
+	IPAddress        []string
+	IPSubnet         []string
+	MACAddress       string
+	TcpWindowSize    int
+	IPEnabled        bool
+}
 
 // interfaceCmd represents the interface command
 var interfaceCmd = &cobra.Command{
 	Use:   "interface",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "get a detailed interface information",
+	Long:  `get a detailed interface information`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("interface called")
+		var dst []interface_win32_NetworkAdapterConfiguration
+
+		q := wmi.CreateQueryFrom(&dst, "Win32_NetworkAdapterConfiguration", "")
+		if err := wmi.Query(q, &dst); err != nil {
+			log.Fatal(err)
+		}
+		for _, v := range dst {
+			if v.IPEnabled {
+				fmt.Printf("Interface Description: %v\n", v.Description)
+				fmt.Printf("IPv4 Address:          %v\n", v.IPAddress[0])
+				fmt.Printf("IPv4 Subnet Mask: %18v\n", v.IPSubnet[0])
+				fmt.Printf("MAC Address: %27v\n", v.MACAddress)
+				fmt.Printf("TCP Window Size:%12v\n\n", v.TcpWindowSize)
+			}
+		}
 	},
 }
 
 func init() {
 	ipCmd.AddCommand(interfaceCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// interfaceCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// interfaceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
